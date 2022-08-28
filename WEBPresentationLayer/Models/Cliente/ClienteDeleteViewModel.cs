9688f61f166e1c0c5bbe3a7800737aa9fe490ac5
Davi Jedrzejczyk <105894355+SprilerBr@users.noreply.github.com>
2022-08-27 22:29:57 -0300

    Delete WEBPresentationLayer directory
    Mudança para Desktop

--- WEBPresentationLayer/Models/Cliente/ClienteDeleteViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WEBPresentationLayer.Models.Cliente
-{
-    public class ClienteDeleteViewModel
-    {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-    }
-}
